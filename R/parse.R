dbb3e2e41759d3577e7c83666a97239612a3849e
Finn Beckmann <fbeckmann.edu@pm.me>
2025-10-02 19:06:00 +0200

    update parsing

--- R/parse.R
+++ R/parse.R
@@ -4,12 +4,6 @@ tnm_parse_json_df <- function(resp) {
   jsonlite::fromJSON(httr2::resp_body_string(resp), simplifyVector = TRUE)
 }
 
-#' @importFrom readr read_csv
-#' @importFrom httr2 resp_body_string
-tnm_parse_csv_df <- function(resp) {
-  readr::read_csv(httr2::resp_body_string(resp), show_col_types = FALSE)
-}
-
 #' @importFrom jsonlite fromJSON
 #' @importFrom httr2 resp_body_string
 #' @importFrom geojsonsf geojson_sf
